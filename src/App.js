5f2ea3b345c2a371fd8817cfceb30205240d757f
pm <pm.gonzaalez@gmail.com>
2022-11-03 13:57:52 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import PostForm from './components/PostForm.js';
 import Home from './components/Home';
+import ListPosts from './components/ListPosts';
 
 
 
@@ -16,6 +17,7 @@ function App() {
         <Route element={<PostForm />} path="/posts" />
         <Route element={<LoginForm />} path="/login" />
         <Route element={<RegisterForm />} path="/register" />
+        <Route element={<ListPosts/>} path="listar"/>
       </Routes >
     </BrowserRouter>
 
---
+++ src/components/ListPosts.js
@@ -0,0 +1,67 @@
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+} from '@chakra-ui/react'
+import Header from './Header.tsx'
+
+import React from 'react';
+
+function ListPosts() {
+    return (
+        <>
+            <Header />
+
+            <TableContainer w="50%"
+                justify={"center"}
+
+                maxW='1044px'
+                mx='auto'
+                ml='33em'
+                mt='2em'>
+                <Table variant='striped' colorScheme='teal'>
+                    <TableCaption>Imperial to metric conversion factors</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>To convert</Th>
+                            <Th>into</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>inches</Td>
+                            <Td>millimetres (mm)</Td>
+                            <Td isNumeric>25.4</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>feet</Td>
+                            <Td>centimetres (cm)</Td>
+                            <Td isNumeric>30.48</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>yards</Td>
+                            <Td>metres (m)</Td>
+                            <Td isNumeric>0.91444</Td>
+                        </Tr>
+                    </Tbody>
+                    <Tfoot>
+                        <Tr>
+                            <Th>To convert</Th>
+                            <Th>into</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                    </Tfoot>
+                </Table>
+            </TableContainer>
+        </>
+    );
+}
+
+export default ListPosts;
