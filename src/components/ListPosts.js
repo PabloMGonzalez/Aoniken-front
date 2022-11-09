113d541d9d76b0e24f9b9edd388b1d42982977c2
pm <pm.gonzaalez@gmail.com>
2022-11-09 14:08:19 -0300

    Update listar posts

--- src/components/ListPosts.js
+++ src/components/ListPosts.js
@@ -44,75 +44,70 @@ function ListPosts() {
 
             {posts && posts.map((post) => (
 
-
-                <Text>{post.title}</Text>
-
-            ))}
-
-            <Center py={6}>
-                <Box
-                    maxW={'330px'}
-                    w={'full'}
-                    bg={useColorModeValue('white', 'gray.800')}
-                    boxShadow={'2xl'}
-                    rounded={'md'}
-                    overflow={'hidden'}>
-                    <Stack
-                        textAlign={'center'}
-                        p={6}
-                        color={useColorModeValue('gray.800', 'white')}
-                        align={'center'}>
-                        <Text
-                            fontSize={'2xl'}
-                            fontWeight={500}
-                            p={2}
-                            px={3}
-                        >
-                            TITULO
-                        </Text>
-                        <Divider />
-                        <Stack direction={'row'} align={'center'} justify={'center'}>
-                            <Text fontSize={'md'}pt={2} >
-                                HOLA HOLA HOLA
+                <Center py={6}>
+                    <Box
+                        maxW={'660px'}
+                        w={'full'}
+                        boxShadow={'2xl'}
+                        rounded={'md'}
+                        overflow={'hidden'}>
+                        <Text p={2}>#{post.id}</Text>
+                        <Stack
+                            textAlign={'center'}
+                            p={6}
+                            align={'center'}>
+                            <Text
+                                fontSize={'2xl'}
+                                fontWeight={500}
+                                p={2}
+                                px={3}
+                            >
+                                {post.title}
                             </Text>
+                            <Divider />
+                            <Stack direction={'row'} align={'center'} justify={'center'}>
+                                <Text fontSize={'md'} pt={2} >
+                                    {post.content}
+                                </Text>
+                            </Stack>
                         </Stack>
-                    </Stack>
+                        <Text>Autor:{post.nombre}</Text>
+                        <Box bg={'gray.300'} px={6} py={10}>
+                            <HStack>
+                                <Button
+                                    w={'full'}
+                                    bg={'green.400'}
+                                    color={'white'}
+                                    rounded={'xl'}
+                                    boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
+                                    _hover={{
+                                        bg: 'green.500',
+                                    }}
+                                    _focus={{
+                                        bg: 'green.500',
+                                    }}>
+                                    Aprobar
+                                </Button>
+                                <Button
+                                    w={'full'}
+                                    bg={'green.400'}
+                                    color={'white'}
+                                    rounded={'xl'}
+                                    boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
+                                    _hover={{
+                                        bg: 'green.500',
+                                    }}
+                                    _focus={{
+                                        bg: 'green.500',
+                                    }}>
+                                    Rechazar
+                                </Button>
+                            </HStack>
 
-                    <Box bg={useColorModeValue('gray.50', 'gray.900')} px={6} py={10}>
-                        <HStack>
-                            <Button
-                                w={'full'}
-                                bg={'green.400'}
-                                color={'white'}
-                                rounded={'xl'}
-                                boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
-                                _hover={{
-                                    bg: 'green.500',
-                                }}
-                                _focus={{
-                                    bg: 'green.500',
-                                }}>
-                                Aprobar
-                            </Button>
-                            <Button
-                                w={'full'}
-                                bg={'green.400'}
-                                color={'white'}
-                                rounded={'xl'}
-                                boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
-                                _hover={{
-                                    bg: 'green.500',
-                                }}
-                                _focus={{
-                                    bg: 'green.500',
-                                }}>
-                                Rechazar
-                            </Button>
-                        </HStack>
-
+                        </Box>
                     </Box>
-                </Box>
-            </Center>
+                </Center>
+            ))}
 
         </>
     );
