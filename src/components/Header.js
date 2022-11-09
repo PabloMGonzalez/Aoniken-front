ab512ce73ab7df1cb7492dc874e87d2e703dd491
pm <pm.gonzaalez@gmail.com>
2022-11-09 10:16:07 -0300

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,82 +28,100 @@ export default function Header() {
   const [isLoggedIn, setIsLoggIn] = useLocalStorage('isLoggedIn');
 
   return (
-    
-      
-      <Box>
-        <Flex
-          bg={useColorModeValue('blue.50', 'gray.800')}
-          color={useColorModeValue('gray.600', 'white')}
-          minH={'60px'}
-          py={{ base: 2 }}
-          px={{ base: 4 }}
-          borderBottom={1}
-          borderStyle={'solid'}
-          borderColor={useColorModeValue('gray.200', 'gray.900')}
-        >
 
-          <Stack
-            ml={'left'}
-            direction={'reverse-row'}
-            spacing={6}>
-            {isLoggedIn && <Button
-              display={{ base: 'none', md: 'inline-flex' }}
-              fontSize={'sm'}
-              fontWeight={600}
-              color={'white'}
-              bg={'blue.400'}
-              _hover={{
-                bg: 'green.300',
-              }}
-              onClick={createPostButton}>
-              Nuevo Posteo
-            </Button>}
-          </Stack>
 
-          <Stack
-            ml={'auto'}
-            direction={'row'}
-            spacing={6}>
-            {!isLoggedIn && <Button
-              display={{ base: 'none', md: 'inline-flex' }}
-              fontSize={'sm'}
-              fontWeight={600}
-              color={'white'}
-              bg={'blue.400'}
-              _hover={{
-                bg: 'green.300',
-              }}
-              onClick={() => navigate("/login/")}>
-              Log In
-            </Button>}
+    <Box>
+      <Flex
+        bg={useColorModeValue('blue.50', 'gray.800')}
+        color={useColorModeValue('gray.600', 'white')}
+        minH={'60px'}
+        py={{ base: 2 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={'solid'}
+        borderColor={useColorModeValue('gray.200', 'gray.900')}
+      >
 
-            {!isLoggedIn && <Button
-              display={{ base: 'none', md: 'inline-flex' }}
-              fontSize={'sm'}
-              fontWeight={600}
-              color={'white'}
-              bg={'blue.400'}
-              _hover={{
-                bg: 'green.300',
-              }}
-              onClick={() => navigate("/register/")}>
-              Registrate
-            </Button>}
+        <Stack
+          ml={'left'}
+          direction={'reverse-row'}
+          spacing={6}>
 
-            {isLoggedIn && <Button
-              display={{ base: 'none', md: 'inline-flex' }}
-              fontSize={'sm'}
-              fontWeight={600}
-              color={'white'}
-              bg={'blue.400'}
-              _hover={{
-                bg: 'green.300',
-              }}
-              onClick={handleLogout}>
-              Log Out
-            </Button>}
-          </Stack>
-        </Flex>
-      </Box>
+          {isLoggedIn && <Button
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'blue.400'}
+            _hover={{
+              bg: 'green.300',
+            }}
+            onClick={() => { navigate('/create_post') }}>
+            Nuevo Posteo
+          </Button>}
+
+          {isLoggedIn && <Button
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            ml="1.7em"
+            bg={'blue.400'}
+            _hover={{
+              bg: 'green.300',
+            }}
+            onClick={() => navigate('/listar_posts')}>
+            Listar Posteos
+          </Button>}
+
+
+
+        </Stack>
+
+        <Stack
+          ml={'auto'}
+          direction={'row'}
+          spacing={6}>
+          {!isLoggedIn && <Button
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'blue.400'}
+            _hover={{
+              bg: 'green.300',
+            }}
+            onClick={() => navigate("/login/")}>
+            Log In
+          </Button>}
+
+          {!isLoggedIn && <Button
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'blue.400'}
+            _hover={{
+              bg: 'green.300',
+            }}
+            onClick={() => navigate("/register/")}>
+            Registrate
+          </Button>}
+
+          {isLoggedIn && <Button
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'blue.400'}
+            _hover={{
+              bg: 'green.300',
+            }}
+            onClick={handleLogout}>
+            Log Out
+          </Button>}
+        </Stack>
+      </Flex>
+    </Box>
   );
 }
